7e32917abe175d795e6ba920cd2eab724b4adb6a
Javier E. Perez P <templeofdvst@gmail.com>
2015-03-11 23:51:34 -0430

    Variable declarations

---
+++ permutas.pl
@@ -0,0 +1,20 @@
+#!/usr/bin/perl
+
+# Se probará:
+# * Intercambio entre MAYUSCULAS y minusculas.
+# * Intercambio entre teclados
+# * t
+
+@dvorak  = qw( ' , . p y f g c r l / = 
+	       a o e u i d h t n s - \ 
+	       ; q j k x b m w v z );
+
+@spanish = qw( q w e r t y u i o p ' + 
+	       a s d f g h j k l ñ { } 
+	       z x c v b n m , . - );
+
+@tokens = qw( . e - { . v );
+
+foreach (@dvorak){
+	print $_ . "\n";
+}
