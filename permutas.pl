e9d8c151358890ce4f4072f3464f13fd10bdd11b
Javier E. Perez P <templeofdvst@gmail.com>
2015-03-14 23:16:16 -0430

    Numbers added, translate 1st arg word, to 2nd arg

--- permutas.pl
+++ permutas.pl
@@ -1,22 +1,35 @@
 #!/usr/bin/perl
 
-# Se probará:
-# * Intercambio entre MAYUSCULAS y minusculas.
-# * Intercambio entre teclados
-# * t
+@querty_latan = qw( ` 1 2 3 4 5 6 7 8 9 0 [ ]
+                    q w e r t y u i o p ' + 
+                    a s d f g h j k l ñ { } 
+                    z x c v b n m , . - ) ;
 
-@dvorak  = qw( ' , . p y f g c r l / = 
-	       a o e u i d h t n s - \ 
-	       ; q j k x b m w v z );
+@dvorak_en  = qw( < 1 2 3 4 5 6 7 8 9 0 ' ¡
+                  ' , . p y f g c r l / = 
+                  a o e u i d h t n s - \ 
+                  ; q j k x b m w v z ) ;
 
-@spanish = qw( q w e r t y u i o p ' + 
-	       a s d f g h j k l ñ { } 
-	       z x c v b n m , . - );
+# @tokens = qw( . e v - { z );
 
-@tokens = qw( . e v - { z );
+# DICTIONARY LOAD q2d ^ d2q
+for ( $i=0 ; $i<= @querty_latan ; $i++ ){
+    $querty2dvorak{$querty_latan[$i]} = $dvorak_en[$i];
+    $dvorak2querty{$dvorak_en[$i]} = $querty_latan[$i];
+}
 
-$palabra = $ARGV[0] ;
+sub translate_keyboard{
+    $word_to_translate = shift; 
+    $convert_to_keyboard = shift; 
+    $translated_word = "" ;
+    @letters = split("",$word_to_translate) ;
+    $convertion = $convert_to_keyboard eq 'q' ? dvorak2querty : querty2dvorak ; 
 
-#foreach (@dvorak){
-#	print $_ . " " .  $palabra . "\n";
-#}
+    for($j=0 ; $j < @letters ; $j++){
+        $translated_word .= $$convertion{$letters[$j]} ;
+    }
+
+    print $translated_word . "\n" ;
+}
+
+&translate_keyboard($ARGV[0],$ARGV[1]) ;
